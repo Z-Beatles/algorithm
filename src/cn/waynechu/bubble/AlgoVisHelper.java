994ed2912e447e1896db2a45048f4c32bd9343a4
Wayne Chu <waynechu@waynechu.cn>
2018-03-22 16:54:29 +0800

    为工具类添加常用颜色常量及绘制矩形的方法

--- src/cn/waynechu/bubble/AlgoVisHelper.java
+++ src/cn/waynechu/bubble/AlgoVisHelper.java
@@ -1,7 +1,9 @@
 package cn.waynechu.bubble;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 
 /**
  * Graphics2d 绘图工具类
@@ -12,6 +14,28 @@ import java.awt.geom.Ellipse2D;
 public class AlgoVisHelper {
     private AlgoVisHelper() {
     }
+
+    public static final Color Red = new Color(0xF44336);
+    public static final Color Pink = new Color(0xE91E63);
+    public static final Color Purple = new Color(0x9C27B0);
+    public static final Color DeepPurple = new Color(0x673AB7);
+    public static final Color Indigo = new Color(0x3F51B5);
+    public static final Color Blue = new Color(0x2196F3);
+    public static final Color LightBlue = new Color(0x03A9F4);
+    public static final Color Cyan = new Color(0x00BCD4);
+    public static final Color Teal = new Color(0x009688);
+    public static final Color Green = new Color(0x4CAF50);
+    public static final Color LightGreen = new Color(0x8BC34A);
+    public static final Color Lime = new Color(0xCDDC39);
+    public static final Color Yellow = new Color(0xFFEB3B);
+    public static final Color Amber = new Color(0xFFC107);
+    public static final Color Orange = new Color(0xFF9800);
+    public static final Color DeepOrange = new Color(0xFF5722);
+    public static final Color Brown = new Color(0x795548);
+    public static final Color Grey = new Color(0x9E9E9E);
+    public static final Color BlueGrey = new Color(0x607D8B);
+    public static final Color Black = new Color(0x000000);
+    public static final Color White = new Color(0xFFFFFF);
 
     /**
      * 设置画笔宽度
@@ -31,7 +55,7 @@ public class AlgoVisHelper {
      * 绘制空心圆
      **/
     public static void strokeCircle(Graphics2D g2d, int x, int y, int r) {
-        Ellipse2D circle = new Ellipse2D.Float(x - r, y - r, 2 * r, 2 * r);
+        Ellipse2D circle = new Ellipse2D.Double(x - r, y - r, 2 * r, 2 * r);
         g2d.draw(circle);
     }
 
@@ -39,24 +63,51 @@ public class AlgoVisHelper {
      * 绘制实心圆
      **/
     public static void fillCircle(Graphics2D g2d, int x, int y, int r) {
-        Ellipse2D circle = new Ellipse2D.Float(x - r, y - r, 2 * r, 2 * r);
+        Ellipse2D circle = new Ellipse2D.Double(x - r, y - r, 2 * r, 2 * r);
         g2d.fill(circle);
     }
 
     /**
      * 绘制空心矩形
      **/
-    public static void strokeRectangle(Graphics2D g2d, int w) {
-
+    public static void strokeRectangle(Graphics2D g2d, int x, int y, int w, int h) {
+        Rectangle2D rectangle = new Rectangle2D.Double(x, y, w, h);
+        g2d.draw(rectangle);
     }
 
     /**
      * 绘制实心矩形
      **/
-    public static void fillRectangle(Graphics2D g2d) {
-
+    public static void fillRectangle(Graphics2D g2d, int x, int y, int w, int h) {
+        Rectangle2D rectangle = new Rectangle2D.Double(x, y, w, h);
+        g2d.fill(rectangle);
     }
 
+    /**
+     * 添加图片
+     **/
+    public static void putImage(Graphics2D g2d, int x, int y, String imageURL) {
+        ImageIcon icon = new ImageIcon(imageURL);
+        Image image = icon.getImage();
+        g2d.drawImage(image, x, y, null);
+    }
+
+    /**
+     * 添加文字
+     **/
+    public static void drawText(Graphics2D g, String text, int centerX, int centerY) {
+        if (text == null) {
+            throw new IllegalArgumentException("text is null!");
+        }
+        FontMetrics metrics = g.getFontMetrics();
+        int w = metrics.stringWidth(text);
+        int h = metrics.getDescent();
+        g.drawString(text, centerX - w / 2, centerY + h);
+    }
+
+    /**
+     * 进程睡眠
+     **/
     public static void pause(long millis) {
         try {
             Thread.sleep(millis);
