0583c0a7d6ecac25d63b3484b5f152b3b483bc65
Wayne Chu <waynechu@waynechu.cn>
2018-03-30 10:29:34 +0800

    实现单链表的快速排序

---
+++ src/cn/waynechu/sort/ListNode.java
@@ -0,0 +1,25 @@
+package cn.waynechu.sort;
+
+/**
+ * @author waynechu
+ * Created 2018-03-29 23:18
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public String printList() {
+        ListNode node = this;
+        StringBuilder builder = new StringBuilder();
+        while (node != null) {
+            builder.append(node.val).append(" - ");
+            node = node.next;
+        }
+        return builder.substring(0, builder.length() - 3);
+    }
+}
+
---
+++ src/cn/waynechu/sort/quick/QuickSortLinkedList.java
@@ -0,0 +1,59 @@
+package cn.waynechu.sort.quick;
+
+import cn.waynechu.sort.ListNode;
+
+/**
+ * 快速排序单向链表
+ *
+ * @author waynechu
+ * Created 2018-03-30 09:33
+ */
+public class QuickSortLinkedList {
+
+    public static void quickSort(ListNode head, ListNode tail) {
+        if (head == null || head == tail) {
+            return;
+        }
+
+        int pivot = head.val;
+        ListNode lt = head;
+        ListNode i = head.next;
+        // 分区 [head...lt] < pivot; [lt.next...tail] >= pivot
+        while (i != tail.next) {
+            if (i.val < pivot) {
+                lt = lt.next;
+                int tmp = i.val;
+                i.val = lt.val;
+                lt.val = tmp;
+            }
+            i = i.next;
+        }
+        head.val = lt.val;
+        lt.val = pivot;
+
+        quickSort(head, lt);
+        quickSort(lt.next, tail);
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(2);
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(8);
+        ListNode node3 = new ListNode(6);
+        ListNode node4 = new ListNode(3);
+        ListNode node5 = new ListNode(7);
+        ListNode node6 = new ListNode(5);
+        ListNode tail = new ListNode(4);
+        head.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
+        node6.next = tail;
+
+        System.out.println(head.printList());
+        quickSort(head, tail);
+        System.out.println(head.printList());
+    }
+}
